e0d56e8dc09133d9a8b176595fd8180a4f1eb8cf
killserver <killeserver@gmail.com>
2014-08-20 15:05:22 +0300

    fix all comments

--- all_comments.php
+++ all_comments.php
@@ -30,54 +30,41 @@ require "include/bittorrent.php";
 
 dbconn();
 
+if (get_user_class() < UC_MODERATOR) 
+stderr("Error", "Ну и куда нас занесло?"); 
+
 //loggedinorreturn();
 
 stdhead("Коментарии");
 
 begin_main_frame();
 
-if (get_user_class() < UC_MODERATOR) 
-stderr("Error", "Ну и куда нас занесло?"); 
-
 $limit = 30;
 
-$res = mysql_query("SELECT * FROM comments ORDER BY id DESC LIMIT $limit") or sqlerr(__FILE__, __LINE__); 
+$res = mysql_query("SELECT *, (SELECT name FROM torrents WHERE id=comments.torrent) AS torrname, (SELECT username FROM users WHERE id = comments.user) AS username_torr FROM comments ORDER BY id DESC LIMIT ".$limit) or sqlerr(__FILE__, __LINE__); 
 print("<h1>Просмотр сообщений</h1>\n"); 
   print("<table border=1 cellspacing=0 cellpadding=5>\n"); 
   print("<tr><td class=colhead align=left>От кого</td><td class=colhead align=left>Торрент</td><td class=colhead>ID торрента</td><td class=colhead align=left>Комментарий</td></tr>\n"); 
-  while ($arr = mysql_fetch_assoc($res)) 
-  { 
-    //$res1 = mysql_query("SELECT name FROM torrents WHERE id=" . $arr["torrent"]) or sqlerr(); 
-    //$arr1 = mysql_fetch_assoc($res1); 
+  while($arr = mysql_fetch_assoc($res)) 
+  {
+    if($arr['torrent']<>0) {
+        $receiver = "<a href=details.php?id=" . $arr["torrent"] . "><b>" .view_saves($arr['torrname']). "</b></a>"; 
+    } else {
+        $receiver = "<a href=futurerldetails.php?id=" . $arr["trailer"] . "><b>" .view_saves($arr['torrname']). "</b></a>"; 
+    } 
+    if($arr['torrent']>0) {
+        $idreceiver = "<a href=details.php?id=" . $arr["torrent"] . "><b>" . $arr["torrent"] . "</b></a>"; 
+    } elseif($arr['video']>0) {
+        $idreceiver = "<a href=details_of_movie.php?id=" . $arr["video"] . "><b>Видео блог №" . $arr["video"] . "</b></a>"; 
+    } else {
+        $idreceiver = "<a href=futurerldetails.php?id=" . $arr["video"] . "><b>Трейлер №" . $arr["video"] . "</b></a>"; 
+    }
 
-    $torr1 = mysql_query("SELECT name FROM torrents WHERE id=" . $arr["torrent"]) or sqlerr(); 
-    $torr1 = mysql_fetch_assoc($torr1); 
-if ($arr['torrent'] <> 0) {
-    $receiver = "<a href=details.php?id=" . $arr["torrent"] . "><b>" .$torr1[name]. "</b></a>"; 
-} else {
-    $receiver = "<a href=futurerldetails.php?id=" . $arr["trailer"] . "><b>" .$torr1[name]. "</b></a>"; 
-}
-
- $res4 = mysql_query("SELECT torrent FROM comments WHERE id=" . $arr["id"]) or sqlerr(); 
-    $arr4 = mysql_fetch_assoc($res4); 
-if ($arr['torrent'] > 0) {
-    $idreceiver = "<a href=details.php?id=" . $arr4["torrent"] . "><b>" . $arr4["torrent"] . "</b></a>"; 
-} elseif ($arr['video'] > 0) {
-    $idreceiver = "<a href=details_of_movie.php?id=" . $arr["video"] . "><b>Видео блог №" . $arr["video"] . "</b></a>"; 
-} else {
-    $idreceiver = "<a href=futurerldetails.php?id=" . $arr["video"] . "><b>Трейлер №" . $arr["video"] . "</b></a>"; 
-}
-
-    $res2 = mysql_query("SELECT torrent FROM comments WHERE id=" . $arr["torrent"]) or sqlerr(); 
-    $arr2 = mysql_fetch_assoc($res2); 
-    $res3 = mysql_query("SELECT username FROM users WHERE id=" . $arr["user"]) or sqlerr(); 
-    $arr3 = mysql_fetch_assoc($res3); 
-    $sender = "<a href=userdetails.php?id=" . $arr["user"] . "><b>" . $arr3["username"] . "</b></a>"; 
+    $sender = "<a href=userdetails.php?id=" . $arr["user"] . "><b>" . $arr["username_torr"] . "</b></a>"; 
     $msg = format_comment($arr["text"]); 
-  print("<tr><td>$sender</td><td>$receiver</td><td>$idreceiver</td><td align=left>$msg</td></tr>\n"); 
+    print("<tr><td>".$sender."</td><td>".$receiver."</td><td>".$idreceiver."</td><td align=\"left\">".$msg."</td></tr>\n"); 
   } 
   print("</table>"); 
-
  end_frame(); 
 
 stdfoot();

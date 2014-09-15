39ce3665782879fb988b0ac238d2138b7561f101
killserver <killeserver@gmail.com>
2014-09-15 22:24:21 +0300

    fix monitor

--- msgmonitor.php
+++ msgmonitor.php
@@ -10,7 +10,7 @@ $userid = (int)$_POST["userid"];
 if(!$tname || !$userid)
 die("Доступ запрещен");	
 
-$subject = "Торрент "$tname" Добавлен";
+$subject = "Торрент ".$tname." Добавлен";
 $msg = "Ваша искомая раздача [b] $tname [/b] была добавлена! Пожалуйста воспользуйтесь поиском и вы ее найдете ";
 sql_query("INSERT INTO messages(sender, receiver, added, subject, msg) VALUES (0, ".sqlesc($userid).", NOW(), ".sqlesc($subject).", ".sqlesc($msg).")");
 sql_query("DELETE from monitor WHERE torrentname=".sqlesc($tname)."");
